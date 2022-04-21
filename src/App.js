88c86b876d6432480669c4805e67dc6ccbd303b5
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2022-04-21 18:31:10 -0500

    Avance

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
 import React from "react";
+import { useEffect } from "react";
+import md5 from "md5";
 import "./App.css";
+import HeroCard from "./components/HeroCard";
+
+const ts = String(10000);
+const preHash = ts + process.env.REACT_APP_MARVEL_PK + process.env.REACT_APP_MARVEL_PUK;
+const URL = "https://gateway.marvel.com:443/v1/public/characters?";
+let data;
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  useEffect(() => {
+    const hash = md5(preHash);
+    if (navigator.onLine) {
+      fetch(
+        URL +
+          new URLSearchParams({
+            ts,
+            apikey: process.env.REACT_APP_MARVEL_PUK,
+            hash,
+            limit: 1,
+          })
+      )
+        .then((res) => res.json())
+        .then((results) => {
+          data = results.data.results;
+          console.log(data);
+          localStorage.setItem("heroes", data);
+        });
+    }
+  }, []);
+
+  if (navigator.onLine) {
+    console.log("Online");
+    return (
+      <React.Fragment>
+        <div className="grid grid-cols-4 gap-4 m-5">
+          <HeroCard
+            name="Test"
+            description="Description Test"
+            image="https://www.fdd.cl/wp-content/uploads/2017/09/Test.jpg"
+          />
+        </div>
+      </React.Fragment>
+    );
+  } else {
+    if (localStorage.getItem("heroes") === null) {
+      return <h1> No Heroes available</h1>;
+    } else {
+      data = localStorage.getItem("heroes");
+      return (
+        <React.Fragment>
+          <div className="grid grid-cols-4 gap-4 m-5">
+            {data.map((hero, key) => {
+              console.log(hero);
+              return (
+                <HeroCard
+                  name={hero.name}
+                  description={hero.description}
+                  image="https://www.fdd.cl/wp-content/uploads/2017/09/Test.jpg"
+                  key={key}
+                />
+              );
+            })}
+          </div>
+        </React.Fragment>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/components/HeroCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HeroCard({ name, description, image }) {
+  return (
+    <React.Fragment>
+      <div className="flex-initial flex-col justify-center w-64 shadow-lg rounded ">
+        <img className="object-cover p-6 h-48 w-96" src={image} alt={name} />
+        <div className="flex justify-center">
+          <p className="text-xl">{name}</p>
+        </div>
+        <div className="flex justify-center pb-6">
+          <p>{description}</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default HeroCard;
